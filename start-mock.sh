f5c42254351d3481d1ede10887df521cf6e5a2fb
Robert Bullen <robert@Roberts-iMac.local>
2020-02-10 20:24:04 -0600

    split start script into two

--- start-mock.sh
+++ start-mock.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-sudo python3 -m interdaktive \
+sudo GPIOZERO_PIN_FACTORY=mock python3 -m interdaktive \
     --control-button-pin=BOARD36 \
-    --display-type=video-core \
+    --display-type=mock \
     --motion-sensor-pin=BOARD7 \
     --motion-led-pin=BOARD22 \
     --running-led-pin=BOARD18 \
-    --sleep-delay-seconds=150 \
+    --sleep-delay-seconds=5 \
     --waking-time-begin=06:00 \
-    --waking-time-end=22:00 &
+    --waking-time-end=22:00
