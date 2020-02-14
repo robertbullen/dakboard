440b9aeca79c0f60ef89b6b40fe41746d479fdc0
Robert Bullen <robert@robertbullen.com>
2020-02-14 17:04:06 -0600

    improved how the state machine operates

--- start.sh
+++ start.sh
@@ -9,9 +9,9 @@ fi
 sudo python3 -m interdaktive \
     --control-button-pin=BOARD36 \
     --display-type=video-core \
-    --idle-timeout-seconds=150 \
     --motion-sensor-pin=BOARD7 \
     --motion-led-pin=BOARD22 \
     --running-led-pin=BOARD18 \
+    --timer-seconds=90 \
     --waking-hours-begin=06:00 \
     --waking-hours-end=22:00 &
