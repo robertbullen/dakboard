531ec1841b4aabaf5d6e7b14addb7fa1d91d9948
Robert Bullen <robert@Roberts-iMac.local>
2020-07-21 14:02:44 -0500

    moved data-src highlighting to top level

--- docs/slides/highlight-data-src.js
+++ docs/slides/highlight-data-src.js
@@ -1,12 +1,13 @@
 window.addEventListener('load', () => {
 	const intervalHandle = window.setInterval(() => {
+		// Look for <code> blocks with a `data-src` attribute.
 		const dataSrcAttr = 'data-src';
-		const codes = document.querySelectorAll(
-			`pre > code.hljs[${dataSrcAttr}]`,
-		);
+		const codes = document.querySelectorAll(`pre > code.hljs[${dataSrcAttr}]`);
+
 		if (codes.length > 0) {
 			window.clearInterval(intervalHandle);
 
+			// Fetch the source code content and highlight it.
 			highlightPlugin = Reveal.getPlugin('highlight');
 			codes.forEach(async (code) => {
 				const response = await fetch(code.dataset.src);
